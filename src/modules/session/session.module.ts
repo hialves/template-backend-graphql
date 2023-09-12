f75a28923b48dbbadc6a97a229bc659251eb2ec6
Hiago Alves <hiagotnt1@gmail.com>
2023-09-11 22:17:20 -0300

    fix: remove User entity from session module

--- src/modules/session/session.module.ts
+++ src/modules/session/session.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SessionService } from './session.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../user/entities/user.entity';
 import { Session } from './entities/session.entity';
 import { CacheModule } from '../../connections/cache/cache.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Session, User]), CacheModule],
+  imports: [TypeOrmModule.forFeature([Session]), CacheModule],
   providers: [SessionService],
   exports: [SessionService],
 })

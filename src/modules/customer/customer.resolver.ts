4bb62e4eb2921fad5c161b466e5f0235e767af85
Hiago Alves <hiagotnt1@gmail.com>
2023-09-27 00:10:28 -0300

    chore: prettier and add return type

--- src/modules/customer/customer.resolver.ts
+++ src/modules/customer/customer.resolver.ts
@@ -1,13 +1,13 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
-import { IsPublic } from '../../decorators/public.decorator'
-import { CreateCustomerInput } from './dto/create-customer.input'
-import { CustomerService } from './customer.service'
-import { Role } from '../../common/enums/role.enum'
-import { Roles } from '../../decorators/roles.decorator'
-import { ID } from '../../@types'
-import { UpdateCustomerInput } from './dto/update-customer.input'
-import { Customer } from './entities/customer.entity'
-import { DeleteResult } from '../../common/graphql/types/result-type'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { IsPublic } from '../../decorators/public.decorator';
+import { CreateCustomerInput } from './dto/create-customer.input';
+import { CustomerService } from './customer.service';
+import { Role } from '../../common/enums/role.enum';
+import { Roles } from '../../decorators/roles.decorator';
+import { ID } from '../../@types';
+import { UpdateCustomerInput } from './dto/update-customer.input';
+import { Customer } from './entities/customer.entity';
+import { DeleteResult } from '../../common/graphql/types/result-type';
 
 @Resolver()
 export class CustomerResolver {
@@ -16,7 +16,7 @@ export class CustomerResolver {
   @IsPublic()
   @Mutation(() => Customer)
   createCustomer(@Args({ name: 'input', type: () => CreateCustomerInput }) input: CreateCustomerInput) {
-    return this.service.create(input)
+    return this.service.create(input);
   }
 
   @Roles(Role.SuperAdmin)
@@ -28,18 +28,18 @@ export class CustomerResolver {
   @Roles(Role.SuperAdmin, Role.Manager, Role.Customer)
   @Query(() => Customer)
   customer(@Args('id') id: ID) {
-    return this.service.findOne(id)
+    return this.service.findOne(id);
   }
 
   @Roles(Role.SuperAdmin, Role.Customer)
   @Mutation(() => Customer)
   updateCustomer(@Args('input') input: UpdateCustomerInput) {
-    return this.service.update(input.id, input)
+    return this.service.update(input.id, input);
   }
 
   @Roles(Role.SuperAdmin)
   @Mutation(() => DeleteResult)
-  removeCustomer(@Args('id') id: ID) {
-    return this.service.remove(id)
+  removeCustomer(@Args('id') id: ID): Promise<DeleteResult> {
+    return this.service.remove(id);
   }
 }
